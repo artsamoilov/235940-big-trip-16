7240abc5cb1e5e13e19690e7a8cbe6d92dfbac07
Artur Samoilov <artsamoilov@gmail.com>
2022-01-16 01:04:11 +0300

    добавляет класс для работы с API

---
+++ src/api-service.js
@@ -0,0 +1,62 @@
+const Method = {
+  GET: 'GET',
+  PUT: 'PUT',
+};
+
+export default class ApiService {
+  #endPoint = null;
+  #authorization = null;
+
+  constructor(endPoint, authorization) {
+    this.#endPoint = endPoint;
+    this.#authorization = authorization;
+  }
+
+  get tripEvents() {
+    return this.#load({url: 'points'}).then(ApiService.parseResponse);
+  }
+
+  #load = async ({
+    url,
+    method = Method.GET,
+    body = null,
+    headers = new Headers(),
+  }) => {
+    headers.append('Authorization', this.#authorization);
+
+    const response = await fetch(
+      `${this.#endPoint}/${url}`,
+      {method, body, headers}
+    );
+
+    try {
+      ApiService.checkStatus(response);
+      return response;
+    } catch (err) {
+      ApiService.catchError(err);
+    }
+  }
+
+  updateTripEvent = async (tripEvent) => {
+    const response = await this.#load({
+      url: `points/${tripEvent.id}`,
+      method: Method.PUT,
+      body: JSON.stringify(tripEvent),
+      headers: new Headers({'Content-Type': 'application/json'}),
+    });
+
+    return await ApiService.parseResponse(response);
+  }
+
+  static parseResponse = (response) => response.json();
+
+  static checkStatus = (response) => {
+    if (!response.ok) {
+      throw new Error(`${response.status}: ${response.statusText}`);
+    }
+  }
+
+  static catchError = (err) => {
+    throw err;
+  }
+}
